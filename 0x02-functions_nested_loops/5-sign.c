437c85729bc24d819aa3655fe26a301e893bf0ae
MissFz <fatimaeddif@gmail.com>
2023-03-21 19:52:08 +0100

    Tasks mandatories are commited

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *print_sign:prints the sign of a number.
- * Return: Always 0.
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
-	}
-	else
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
 	}
-	 _putchar('\n');
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
 }
